245e36686a6cb3f7fba9ed639fabfe7faa0d5ac3
Ben Brown <bentombrown@gmail.com>
2025-08-15 15:25:35 -0500

    Working on UI

---
+++ Assets/Scripts/PlayablePokemon.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayablePokemon", menuName = "Scriptable Objects/PlayablePokemon")]
+public class PlayablePokemon : ScriptableObject
+{
+    
+    public float AttackDamage = 0;
+}
